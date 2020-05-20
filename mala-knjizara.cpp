8fe217cee2c2e07c97f0eb42e4ff7c0c76badd8d
JosipDecur <64425344+JosipDecur@users.noreply.github.com>
2020-05-20 18:02:02 +0200

    Osnovni kod
    Dodani su izbori potrebni za rad knjižnice

--- mala-knjizara.cpp
+++ mala-knjizara.cpp
@@ -5,24 +5,59 @@
 #include <string>
 #include <utility>
 
+using namespace std;
+
 int main()
 {
-    int izbor, brojac=0;
-    int brArtikla=0;
+    int izbor;
+    int brKnjige=0;
     unsigned long long int *barkod = new unsigned long long int[2000];
     string *naziv = new string[2000];
     double *cijena = new double[2000];
     while(1)
     {
-        cout << "1. Unos novog artikla: " << endl;
+        cout << "1. Unos nove knjige: " << endl;
         cout << "2. Ispis svih podataka: " << endl;
-        cout << "3. Inventura: " << endl;
-        cout << "4. Pretraga prema nazivu artikla: " << endl;
-        cout << "5. Brisanje artikla: " << endl;
-        cout << "6. Izmjena podataka artikla: " << endl;
+        cout << "3. Kupnja knjiga: " << endl;
+        cout << "4. Pretraga prema nazivu knjige: " << endl;
+        cout << "5. Brisanje knjige: " << endl;
+        cout << "6. Izmjena podataka knjige: " << endl;
         cout << "7. Izlaz iz programa: " << endl;
         cout << "Odaberite: ";
-        cin >> izbor:
+        cin >> izbor;
+        if(izbor==1)
+        {
+            unsigned long long unos;
+            while(1)
+            {
+                try
+                {
+                    cout << "Unesite barkod: ";
+                    cin >> unos;
+                    if(unos<100000 || unos>999999)
+                        throw "Barkod mora imati 6 znamenki.";
+                    break;
+                }
+                catch (const char* msg)
+                {
+                    cout << msg << endl;
+                }
+            }
+            barkod[brKnjige]=unos;
+            cout << "Unesite naziv knjige: " << endl;
+            cin.ignore();
+            getline(cin, naziv[brKnjige]);
+            cout << "Unesite cijenu knjige: " << endl;
+            cin >> cijena[brKnjige];
+            brKnjige++;
+        }
+        else if(izbor==2)
+        {
+            for(int i=0;i<brKnjige;i++)
+            {
+                cout << barkod[i] << ", " << naziv[i] << ", " << cijena[i] << endl;
+            }
+        }
     }
     return 0;
 }
